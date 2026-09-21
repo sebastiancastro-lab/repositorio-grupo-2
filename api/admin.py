b929cd05470ef414bb331edbe3705c7b7ec22264
lizethgiraldoz <lizeth.giraldoz@udea.edu.co>
2026-09-20 22:53:10 -0500

    feat: validaciones, admin y tests de TipoSigno

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TipoSigno
 
-# Register your models here.
+@admin.register(TipoSigno)
+class TipoSignoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'unidad_medida', 'valor_min_normal', 'valor_max_normal')
+    search_fields = ('nombre',)
