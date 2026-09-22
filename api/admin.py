36d3d495e38790940ec59cce9cf135435fe9260a
lizethgiraldoz <lizeth.giraldoz@udea.edu.co>
2026-09-21 19:46:06 -0500

    RegistroSigno admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,4 @@
-from django.contrib import admin
-
-# Register your models here.
+@admin.register(RegistroSigno)
+class RegistroSignoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'paciente', 'tipo_signo', 'dispositivo', 'valor_medido', 'fecha_hora', 'responsable')
+    list_filter = ('tipo_signo', 'dispositivo')
